8664d0eec9945d3ed103043017d5e9d92165f0b9
Fellipe Santos <fellipeuker12@gmail.com>
2025-06-21 23:33:11 -0300

    card de detalhes dos paises finalizado

--- lista-paises/src/app/detalhe/[code]/_components/cardDetail.tsx
+++ lista-paises/src/app/detalhe/[code]/_components/cardDetail.tsx
@@ -1,13 +1,15 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import { ArrowLeft } from 'lucide-react';
 
-interface cardDetailProps {
+interface CardDetailProps {
   cca3: string;
   flags: { png: string; svg: string; alt?: string };
   name: { common: string };
   capital: string[];
   region: string;
-  population: string;
-  languages: string;
+  population: string; // já formatado
+  languages: string; // já formatado
 }
 
 export const CardDetail = ({
@@ -17,28 +19,32 @@ export const CardDetail = ({
   region,
   languages,
   population,
-}: cardDetailProps) => {
+}: CardDetailProps) => {
   return (
-    <div className="w-full">
-      <h1 className="text-center mt-8">{name.common}</h1>
-      <div>
-        <Link href="/">voltar</Link>
-        <div>
-          <ul>
-            <li>
-              🏙️Capital: <p>{capital}</p>
-            </li>
-            <li>
-              🗺️Continente: <p>{region}</p>
-            </li>
-            <li>
-              👨🏽‍👩🏽‍👧🏽‍👦🏽População: <p>{population}</p>
-            </li>
-            <li>
-              🗣️Linguas faladas: <p>{languages}</p>
-            </li>
-          </ul>
-        </div>
+    <div className="w-full p-4">
+      <h1 className="text-center mt-8 text-3xl font-bold">{name.common}</h1>
+      <Link href="/" className="text-black flex items-center gap-1">
+        <ArrowLeft size={16} /> Voltar
+      </Link>
+      <div className="w-full rounded-2xl mt-4 flex items-center justify-around gap-2 bg-white p-8">
+        <ul className="space-y-1">
+          <li>🏙️ Capital: {capital.join(', ')}</li>
+          <li>🗺️ Continente: {region}</li>
+          <li>👨‍👩‍👧‍👦 População: {population}</li>
+          <li className="flex flex-col gap-2">
+            🗣️ Línguas faladas:{' '}
+            <p className="bg-blue-700 rounded-xl w-16 p-1 text-center text-white text-[12px] font-normal">
+              {languages}
+            </p>
+          </li>
+        </ul>
+        <Image
+          src={flags.png}
+          width={200}
+          height={120}
+          alt={flags.alt || name.common}
+          className="mt-4 rounded"
+        />
       </div>
     </div>
   );
